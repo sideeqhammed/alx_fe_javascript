74f7d9cc930d9e21e4f942bcd6f565ee67c6e095
sideeqhammed <sideeqhammed05.com>
2025-08-22 17:57:58 +0100

    added dom-manipulation task

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,48 +1,48 @@
 let quotes = [
-  {
-    text: "The only way to do great work is to love what you do.",
-    category: "inspirational"
-  },
-  {
-    text: "Innovation distinguishes between a leader and a follower.",
-    category: "technology"
-  },
-  {
-    text: "You miss 100% of the shots you don't take.",
-    category: "inspirational"
-  },
-  {
-    text: "The best way to predict the future is to invent it.",
-    category: "technology"
-  },
-  {
-    text: "I have not failed. I've just found 10,000 ways that won't work.",
-    category: "inspirational"
-  },
-  {
-    text: "The journey of a thousand miles begins with a single step.",
-    category: "inspirational"
-  },
-  {
-    text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    category: "inspirational"
-  },
-  {
-    text: "The only place where success comes before work is in the dictionary.",
-    category: "success"
-  },
-  {
-    text: "The future belongs to those who believe in the beauty of their dreams.",
-    category: "inspirational"
-  },
-  {
-    text: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
-    category: "wisdom"
-  },
-  {
-    text: "Change is the law of life. And those who look only to the past or present are certain to miss the future.",
-    category: "change"
-  }
+  // {
+  //   text: "The only way to do great work is to love what you do.",
+  //   category: "inspirational"
+  // },
+  // {
+  //   text: "Innovation distinguishes between a leader and a follower.",
+  //   category: "technology"
+  // },
+  // {
+  //   text: "You miss 100% of the shots you don't take.",
+  //   category: "inspirational"
+  // },
+  // {
+  //   text: "The best way to predict the future is to invent it.",
+  //   category: "technology"
+  // },
+  // {
+  //   text: "I have not failed. I've just found 10,000 ways that won't work.",
+  //   category: "inspirational"
+  // },
+  // {
+  //   text: "The journey of a thousand miles begins with a single step.",
+  //   category: "inspirational"
+  // },
+  // {
+  //   text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  //   category: "inspirational"
+  // },
+  // {
+  //   text: "The only place where success comes before work is in the dictionary.",
+  //   category: "success"
+  // },
+  // {
+  //   text: "The future belongs to those who believe in the beauty of their dreams.",
+  //   category: "inspirational"
+  // },
+  // {
+  //   text: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+  //   category: "wisdom"
+  // },
+  // {
+  //   text: "Change is the law of life. And those who look only to the past or present are certain to miss the future.",
+  //   category: "change"
+  // }
 ];
 const saveQuotes = () => {
   localStorage.setItem('quotesArr', JSON.stringify(quotes))
@@ -58,6 +58,8 @@ const displayQuote = document.getElementById('quoteDisplay');
 const button1 = document.getElementById('newQuote');
 const addNewQuote = document.getElementById('addNewQuote');
 const body = document.querySelector('body');
+const exportQuotes = document.getElementById('exportQuotes');
+const importQuotes = document.getElementById('importFile')
 
 
 const showRandomQuote = () => {
@@ -106,10 +108,42 @@ const createAddQuoteForm = () => {
   //   <button onclick="addQuote()">Add Quote</button>
 }
 
-addNewQuote.addEventListener('click', (createAddQuoteForm))
+const exportToJsonFile = () => {
+  const exportQuotesJson = JSON.stringify(quotes);
+  const blob = new Blob([exportQuotesJson], {type: 'application/json'});
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url; a.download = 'quotes.json'
+  body.appendChild(a);
+  a.click();
+  body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
+const importFromJsonFile = (event) => {
+  const fileReader = new FileReader();
+  fileReader.onload = (event) => {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert('Quotes imported successfully!');
+  }
+  fileReader.readAsText(event.target.files[0]);
+}
+
+addNewQuote.addEventListener('click', createAddQuoteForm, {once: true})
 
 console.log(quotes);
 
 button1.addEventListener('click', showRandomQuote);
+
+exportQuotes.addEventListener('click', exportToJsonFile);
+
+importQuotes.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    importFromJsonFile;
+  }
+})
 
 // localStorage.clear()

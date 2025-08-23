d4b2502ead5530407159128f2e187457785b657c
sideeqhammed <sideeqhammed05.com>
2025-08-23 15:46:02 +0100

    added dom-manipulation task

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -210,7 +210,7 @@ clearQuotes.addEventListener('click', () => {
 })
 
 
-const fetchPosts = async() => {
+const fetchQuotesFromServer = async() => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     if (!response.ok) {
